162abe6a9097aad3e2738cd7112fbb3dd3acb2d3
scriptsmay <scriptsmay@163.com>
2024-06-17 15:31:06 +0800

    更新tvbox镜像和容器

--- linux/xiaoya/update_image.sh
+++ linux/xiaoya/update_image.sh
@@ -7,52 +7,18 @@
 # curl -sSL https://raw.githubusercontent.com/scriptsmay/tamper-scripts/main/linux/xiaoya/update_image.sh | sh
 
 # 自定义镜像名称
-image_name=registry.cn-hangzhou.aliyuncs.com/virola/xiaoyaliu_alist:latest
+# image_name=registry.cn-hangzhou.aliyuncs.com/virola/xiaoyaliu_alist:latest
+image_name=xiaoyaliu/alist:latest
 config_dir=/Users/virola/workspace/docker/xiaoya
 
 
 # 定义消息推送API的URL
 API_URL="https://vercel.virola.me/api/send"
 SEND_KEY=""
-DOCKER_USERNAME=""
-DOCKER_PASSWORD=""
 
 if [[ -f $config_dir/tg_send_key.txt ]] && [[ -s $config_dir/tg_send_key.txt ]]; then
     SEND_KEY=$(cat $config_dir/tg_send_key.txt)
 fi
-
-# 是否登录指定镜像仓库
-if [[ -f $config_dir/docker_user.txt ]] && [[ -s $config_dir/docker_user.txt ]]; then
-    DOCKER_USERNAME=$(head -n1 $config_dir/docker_user.txt)
-    DOCKER_PASSWORD=$(head -n2 $config_dir/docker_user.txt)
-fi
-
-function SEND_MSG() {
-    local TEXT="$1"
-
-    # 判断是否设置了SEND_KEY
-    [[ -z "$SEND_KEY" ]] && return 0
-
-    # 获取当前日期和时间，并格式化
-    current_time=$(date "+%Y-%m-%d %H:%M:%S")
-
-    # 使用curl命令发送POST请求
-    # 注意：如果API需要认证或其他特定的头部信息，请在这里添加
-    PAYLOAD='{"text":"'$TEXT'\nTimestamp: '$current_time'","sendkey":"'$SEND_KEY'"}'
-
-    RESPONSE_TEXT=$(curl -s -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$API_URL")
-
-    # 检查curl请求是否成功
-    if [ $? -ne 0 ]; then
-        echo "curl请求失败。" >&2
-        exit 1
-    fi
-}
-
-# 如果DOCKER_USERNAME 有值则
-if [[ ! -z "$DOCKER_USERNAME" ]]; then
-    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD registry.cn-hangzhou.aliyuncs.com
-fi 
 
 # 更新容器
 docker stop xiaoya 2>/dev/null
@@ -70,5 +36,29 @@ fi
 # 更新数据
 # bash -c "$(curl -sSL http://docker.xiaoya.pro/update_data.sh)" -s --no-upgrade
 docker start xiaoya
+
+# 发送通知
+function SEND_MSG() {
+    local TEXT="$1"
+
+    # 判断是否设置了SEND_KEY
+    [[ -z "$SEND_KEY" ]] && return 0
+
+    # 获取当前日期和时间，并格式化
+    current_time=$(date "+%Y-%m-%d %H:%M:%S")
+
+    # 使用curl命令发送POST请求
+    # 注意：如果API需要认证或其他特定的头部信息，请在这里添加
+    PAYLOAD='{"text":"'$TEXT'\n\nTimestamp: '$current_time'","sendkey":"'$SEND_KEY'"}'
+
+    RESPONSE_TEXT=$(curl -s -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$API_URL")
+
+    # 检查curl请求是否成功
+    if [ $? -ne 0 ]; then
+        echo "curl请求失败。" >&2
+        exit 1
+    fi
+}
+
 
 SEND_MSG "docker xiaoya 更新完成"
