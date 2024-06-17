22bbb7c5cc504ac788b1a23ea3212ac29eea1457
scriptsmay <scriptsmay@163.com>
2024-06-17 15:53:39 +0800

    update file

--- linux/xiaoya/update_tvbox.sh
+++ linux/xiaoya/update_tvbox.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#/bin/bash
 
 # 自定义配置目录
 BASE_DIR=/Users/virola/workspace/docker/xiaoya-tvbox
@@ -12,16 +12,15 @@ MOUNT=""
 
 UPDATE=true
 
-echo -e "\e[36m使用配置目录：\e[0m $BASE_DIR"
-echo -e "\e[36m端口映射：\e[0m $PORT1:4567  $PORT2:80"
-
-echo -e "\e[33m默认端口变更为4567\e[0m"
+echo "使用配置目录：$BASE_DIR"
+echo "端口映射： $PORT1:4567 | $PORT2:80"
 
 mkdir -p $BASE_DIR
 
 if ! grep "access.mypikpak.com" /etc/hosts >/dev/null
+    echo "nothing..."
 then
-    echo -e "127.0.0.1\taccess.mypikpak.com" >> /etc/hosts
+    echo "127.0.0.1\taccess.mypikpak.com" >> /etc/hosts
 fi
 
 docker container prune -f --filter "label=MAINTAINER=Har01d"
@@ -33,12 +32,12 @@ ARCH=$(uname -m)
 if [ "$ARCH" = "armv7l" ]; then
   echo "不支持的平台"
   exit 1
-elif [ "$ARCH" = "aarch64" ]; then
+elif [[ "$ARCH" = "aarch64" || "$ARCH" = "arm64" ]]; then
     platform="linux/arm64"
 fi
 
 IMAGE_ID=$(docker images -q haroldli/xiaoya-tvbox:${TAG})
-echo -e "\e[32m下载最新Docker镜像，平台：${platform}, image tag: ${TAG}\e[0m"
+echo "下载最新Docker镜像，平台：${platform}, image tag: ${TAG}"
 for i in 1 2 3 4 5
 do
    docker pull --platform ${platform} haroldli/xiaoya-tvbox:${TAG} && break
@@ -46,35 +45,17 @@ done
 
 NEW_IMAGE=$(docker images -q haroldli/xiaoya-tvbox:${TAG})
 if [ "$UPDATE" = "true" ] && [ "$IMAGE_ID" = "$NEW_IMAGE" ]; then
-  echo -e "\e[33m镜像没有更新\e[0m"
+  echo "镜像没有更新"
   exit
 fi
 
-echo -e "\e[33m重启应用\e[0m"
+echo "重启应用"
 docker rm -f xiaoya-tvbox 2>/dev/null
 docker run -d -p $PORT1:4567 -p $PORT2:80 -e ALIST_PORT=$PORT2 -v "$BASE_DIR":/data $MOUNT --restart=always --name=xiaoya-tvbox haroldli/xiaoya-tvbox:${TAG}
 
 
-echo -e "\n\e[32m请使用以下命令查看日志输出：\e[0m"
-echo -e "    docker logs -f xiaoya-tvbox\n"
-
-IP=$(ip a | grep -F '192.168.' | awk '{print $2}' | awk -F/ '{print $1}' | head -1)
-if [ -n "$IP" ]; then
-  echo ""
-  echo -e "\e[32m请用以下地址访问：\e[0m"
-  echo -e "    \e[32m管理界面\e[0m： http://$IP:$PORT1/"
-  echo -e "    \e[32m小雅AList\e[0m： http://$IP:$PORT2/"
-else
-  IP=$(ip a | grep -F '10.' | awk '{print $2}' | awk -F/ '{print $1}' | grep -E '\b10.' | head -1)
-  if [ -n "$IP" ]; then
-    echo ""
-    echo -e "\e[32m请用以下地址访问：\e[0m"
-    echo -e "    \e[32m管理界面\e[0m： http://$IP:$PORT1/"
-    echo -e "    \e[32m小雅AList\e[0m： http://$IP:$PORT2/"
-  fi
-  echo -e "\e[32m云服务器请用公网IP访问\e[0m"
-fi
-echo ""
-
-echo -e "\e[33m默认端口变更为4567\e[0m"
+echo "\n请使用以下命令查看日志输出："
+echo "    docker logs -f xiaoya-tvbox\n"
+echo "\n"
+echo "默认端口变更为4567"
 
