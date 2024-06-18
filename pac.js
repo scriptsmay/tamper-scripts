87c0112d80453f66474c8f94bcc74e130917014b
scriptsmay <scriptsmay@163.com>
2024-06-18 10:04:47 +0800

    update

--- pac.js
+++ pac.js
@@ -1,14 +1,18 @@
 function FindProxyForURL(url, host) {
     var noproxy =
         'localhost,192.168.*,127.0.0.1,*.local,timestamp.apple.com,sequoia.apple.com,seed-sequoia.siri.apple.com';
-    noproxy.split(',').forEach(function (item) {
+    var isDirect = false;
+    var list = noproxy.split(',');
+    for (let index = 0; index < list.length; index++) {
+        var item = list[index];
         if (shExpMatch(host, item)) {
-            return 'DIRECT';
+            isDirect = true;
+            break;
         }
-    });
+    }
 
     // 如果请求的主机是localhost，则直接连接，不使用代理
-    if (host === 'localhost') {
+    if (isDirect || host === 'localhost') {
         return 'DIRECT';
     }
 
